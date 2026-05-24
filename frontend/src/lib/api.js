790f7ab94d3fd77bd9c9d496ca7388436609fdcc
Paul Doucette <pauldoucette@Kates-Air.lan>
2026-05-23 22:13:24 -0500

    Use same-origin API in production

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://127.0.0.1:8000'
+const API_BASE = import.meta.env.PROD ? '' : 'http://127.0.0.1:8000'
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
